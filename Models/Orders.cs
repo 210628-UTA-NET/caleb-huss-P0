4caaca90d27713300f247268d33a98f208b11a32
Caleb-Huss <caleb.huss97@gmail.com>
2021-06-30 13:45:00 -0500

    Added order properties

--- Models/Orders.cs
+++ Models/Orders.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 namespace Models
 {
     public class Orders
     {
-        private List<Orders> _
+        private List<Orders> _orders;
+        private string _location;
+        private double _totalprice;
+
+        public string Location
+        {
+            get
+            {
+                return _location;
+            }
+            set
+            {
+                _location = value;
+            }
+        }
+        public double TotalPrice 
+        { 
+            get
+            {
+                return _totalprice;
+            } 
+        }
+
     }
 }
