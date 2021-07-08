0ed74cc9a2b164c35970272a11ef63f86471ee2e
Caleb-Huss <caleb.huss97@gmail.com>
2021-07-08 16:56:36 -0500

    Added connection to database via this file

--- UI/MenuFactory.cs
+++ UI/MenuFactory.cs
@@ -1,5 +1,9 @@
+using System.IO;
 using BL;
 using DL;
+using DL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace UI
 {
@@ -7,6 +11,18 @@ namespace UI
     {
         public IMenu GetMenu(MenuType p_menu)
         {
+            //Get the configuration from the appsetting.json file
+            var configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsetting.json")
+                        .Build();
+
+            // Get the connectionString from appsetting
+            string connectionString = configuration.GetConnectionString("Reference2DB");
+            DbContextOptions<DemoDBContext> option = new DbContextOptionsBuilder<DemoDBContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+
             switch (p_menu)
             {
 
