ceec9af2030e4a61d8747b4bfe2412433dec088a
Caleb-Huss <caleb.huss97@gmail.com>
2021-07-08 16:49:17 -0500

    Updated DL to work with MS database

--- DL/CustomerRepository.cs
+++ DL/CustomerRepository.cs
@@ -1,37 +1,49 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
+using System.Linq;
 using Models;
 
 namespace DL
 {
     public class CustomerRepository : ICustomerRepository
     {
-
-        private const string _filePath = "./../DL/Database/Customers.json";
-        private string _jsonString;
+        private Entities.DemoDBContext _context;
+        public CustomerRepository(Entities.DemoDBContext p_context)
+        {
+            _context = p_context;
+        }
         public Customers AddCustomer(Customers p_cust)
         {
-            List<Customers> listOfCustomers = this.GetAllCustomers();
-            listOfCustomers.Add(p_cust);
-            _jsonString = JsonSerializer.Serialize(listOfCustomers, new JsonSerializerOptions{WriteIndented = true});
-            File.WriteAllText(_filePath,_jsonString);
+            // Need to query and get largest customer number then add 1
+            int custNumber = 10;
+            _context.Customers.Add(new Entities.Customer
+            {
+                CustomerId = (int)custNumber + 1,
+                Name = p_cust.Name,
+                Address = p_cust.Address,
+                City = p_cust.City,
+                State = p_cust.State,
+                Email = p_cust.Email,
+                PhoneNumber = p_cust.PhoneNumber
+            });
             return p_cust;
         }
 
         public List<Customers> GetAllCustomers()
         {
-            try
-            {
-                _jsonString = File.ReadAllText(_filePath);
-            }
-            catch (System.Exception)
-            {
-                
-                throw new Exception("Something went wrong while trying to open the Customer DB");
-            }
-            return JsonSerializer.Deserialize<List<Customers>>(_jsonString);
+            return _context.Customers.Select(
+                rest =>
+                    new Customers()
+                    {
+                        CustomerNumber = rest.CustomerId,
+                        Name = rest.Name,
+                        City = rest.City,
+                        State = rest.State,
+                        Address = rest.Address,
+                        Email = rest.Email,
+                        PhoneNumber = (int)rest.PhoneNumber
+                    }
+            ).ToList();
         }
 
         public Customers GetCustomers(Customers p_cust)
