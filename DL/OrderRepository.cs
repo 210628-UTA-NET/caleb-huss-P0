a105bbef3e5dca009ff60594346e0a664e19f1be
Caleb-Huss <caleb.huss97@gmail.com>
2021-07-12 20:37:56 -0500

    Created BL and DL for orders

---
+++ DL/OrderRepository.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Models;
+
+namespace DL
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private Entities.DemoDBContext _context;
+        public OrderRepository(Entities.DemoDBContext p_context)
+        {
+            _context = p_context;
+        }
+        public string AddOrder(Orders p_order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Orders> GetOrders(StoreFront p_store)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Orders> GetOrders(Customers p_cust)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Orders> GetOrders(StoreFront p_store, Customers p_cust)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
